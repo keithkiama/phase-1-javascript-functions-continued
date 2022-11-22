3064c22dc366cd4e54165e17db7366a76a29c23c
keithkiama <keithkiama2003@gmail.com>
2022-11-22 20:49:09 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // code your solution here
+function saturdayFun (activity = "roller-skate") {
+    return `This Saturday, I want to ${activity}!`
+}
+
+const mondayWork = function (activity = "go to the office") {
+    return `This Monday, I will ${activity}.`
+}
+
+function wrapAdjective (val = "*") {
+    const inF = function (value = "||") {
+        return `You are ${val}${value}${val}!`
+    }
+    return inF;
+}
